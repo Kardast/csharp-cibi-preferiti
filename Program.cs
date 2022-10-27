4d5fd6d5c08cd5ce3b5bc939cbe7d30cfb94ce25
Kardast <dottorsf@gmail.com>
2022-10-27 10:23:11 +0200

    Esercizio primi 4 punti

--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-string[] cibiPreferiti = { "Pasta", "Pizza" };
+string[] cibiPreferiti = { "Pasta", "Pizza", "Sushi", "Lasagne", "Cioccolata", "Nocciole", "Mandorle" };
+
+Console.WriteLine(cibiPreferiti.Length);
+for (int i = 0; i < cibiPreferiti.Length; i++)
+{
+    Console.WriteLine(cibiPreferiti[i] + ", posizione: " + i);
+}
+
+Console.WriteLine("Cibo top: " + cibiPreferiti.First());
+Console.WriteLine("Cibo bottom: " + cibiPreferiti.Last());
